1cf9b9a33a9271474d45f1e160f8c3f3372dd54c
vasiliyalex <vasiliyalexsandrovich@gmail.com>
2025-07-13 21:26:05 +0500

    add search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CardList } from './components/CardList';
+import { Search } from './components/Search';
 import './App.css';
 
 interface State {
@@ -13,9 +14,14 @@ export class App extends React.Component<Record<string, never>, State> {
     triggerError: false,
   };
 
+  handleSearch = (query: string) => {
+    this.setState({ query });
+  };
+
   render() {
     return (
       <div className="flex flex-col min-h-screen">
+        <Search onSearch={this.handleSearch} />
         <CardList search={this.state.query} />
       </div>
     );
